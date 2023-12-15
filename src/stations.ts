f85677fe5cf7a1e183cf40912d9bcabdc7e8bfdb
Anton Karpov <karpoff@bk.ru>
2023-12-15 22:32:09 +0400

    new stations

--- src/stations.ts
+++ src/stations.ts
@@ -1,10 +1,12 @@
 const stations = [
     {
+        // Gyumri2
         key: 'A422227',
         title: 'Gyumri',
         disabled: false,
     },
     {
+        // Gyumri
         key: 'A420265',
         title: 'Gyumri',
         disabled: false,
@@ -48,7 +50,7 @@ const stations = [
     {
         // eng city
         key: 'A397555',
-        title: '',
+        title: 'Engineering City',
         disabled: false,
     },
     {
@@ -61,6 +63,66 @@ const stations = [
         // Malatia-Sebastia
         key: 'A407623',
         title: 'Malatia-Sebastia',
+        disabled: false,
+    },
+    {
+        // tsahkadzor
+        key: 'A209092',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Circus
+        key: 'A407629',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Tigran Mets
+        key: 'A407620',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Nzhdeh
+        key: 'A416629',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Hrachya Nersisyan
+        key: 'A462832',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Safaryan new
+        key: 'A414613',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Shorvoyan
+        key: 'A462514',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Adonts
+        key: 'A462409',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Muradyan
+        key: 'A460264',
+        title: '',
+        disabled: false,
+    },
+    {
+        // Sofia
+        key: 'A414688',
+        title: '',
         disabled: false,
     },
 ];
